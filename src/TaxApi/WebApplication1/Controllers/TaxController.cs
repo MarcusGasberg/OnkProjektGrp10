c87adc848036f02062dcddf0638d7bd7e3b95da8
Laurits Dyrberg Hansen <201706994@post.au.dk>
2020-04-27 13:34:36 +0200

    Added TaxController

---
+++ src/TaxApi/WebApplication1/Controllers/TaxController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TaxApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxController : ControllerBase
+    {
+        private readonly ILogger<TaxController> _logger;
+
+        public TaxController(ILogger<TaxController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        [Route("tax")]
+        public Tax CalTax(int money) {
+
+            var tax = new Tax(money/100);
+
+            return tax;
+        }
+
+
+
+    }
+}
