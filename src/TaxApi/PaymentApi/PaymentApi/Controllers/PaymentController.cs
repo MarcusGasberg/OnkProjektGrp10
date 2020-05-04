37a952fd205f91982c83c293e5132e1da29f0caa
Laurits Dyrberg Hansen <201706994@post.au.dk>
2020-05-04 14:35:09 +0200

    added payment, with todo

---
+++ src/TaxApi/PaymentApi/PaymentApi/Controllers/PaymentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TaxApi.Controllers;
+
+namespace PaymentApi.Controllers
+{
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private TaxController tax;
+
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Payment GetPayment()
+        {
+            //get på account i user
+
+
+
+            return 
+        }
+    }
+}
