ce8cfa1fc30fa19671ce4be6ea42e86c3ef263f6
Marcus Gasberg <mrgasberg@hotmail.com>
2020-05-15 14:41:16 +0200

    Add identity sql server

---
+++ src/Services/AccountService/Config.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AccountService
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            {
+                new ApiResource("taxingController", "Taxing Controller"),
+                new ApiResource("paymentController", "Payment Controller"),
+                new ApiResource("stockMarketController", "Stock Market Controller")
+            };
+
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // client credentials flow client
+                new Client
+                {
+                    ClientId = "client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "api1" }
+                },
+                new Client
+                {
+                    ClientId = "angularClient",
+                    ClientName = "Angular Client",
+                    ClientUri = "http://localhost:4200",
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    RedirectUris =
+                    {
+                        "http://localhost:4200",
+                        "http://localhost:4200/callback",
+                    },
+
+                    PostLogoutRedirectUris = { "http://localhost:4200" },
+                    AllowedCorsOrigins = { "http://localhost:4200" },
+
+                    AllowAccessTokensViaBrowser = true,
+                    AccessTokenLifetime = 3600,
+
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "taxingController",
+                        "paymentController",
+                        "stockMarketController"
+                    }
+                }
+            };
+    }
+}
---
+++ src/Services/AccountService/SeedData.cs
@@ -0,0 +1,180 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Linq;
+using System.Security.Claims;
+using IdentityModel;
+using AccountService.Data;
+using AccountService.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Interfaces;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.EntityFramework.Storage;
+
+namespace AccountService
+{
+    public class SeedData
+    {
+        public static void EnsureSeedData(string connectionString)
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+
+            services.AddOperationalDbContext(options =>
+            {
+                options.ConfigureDbContext = db => db.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName));
+            });
+
+            services.AddConfigurationDbContext(options =>
+            {
+                options.ConfigureDbContext = db => db.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(typeof(SeedData).Assembly.FullName));
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(connectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                    context.Database.Migrate();
+
+                    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                    SeedIdentityUsers(context, userManager);
+
+                    scope.ServiceProvider.GetService<PersistedGrantDbContext>().Database.Migrate();
+
+                    var configurationContext = scope.ServiceProvider.GetService<ConfigurationDbContext>();
+                    configurationContext.Database.Migrate();
+                    SeedConfigurations(configurationContext);
+                }
+            }
+        }
+
+        private static void SeedIdentityUsers(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            var alice = userManager.FindByNameAsync("alice").Result;
+            if (alice == null)
+            {
+                alice = new ApplicationUser
+                {
+                    UserName = "alice"
+                };
+                var result = userManager.CreateAsync(alice, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userManager.AddClaimsAsync(alice, new Claim[]{
+                new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                new Claim(JwtClaimTypes.GivenName, "Alice"),
+                new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
+                new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+            }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+                Log.Debug("alice created");
+            }
+            else
+            {
+                Log.Debug("alice already exists");
+            }
+
+            var bob = userManager.FindByNameAsync("bob").Result;
+            if (bob == null)
+            {
+                bob = new ApplicationUser
+                {
+                    UserName = "bob"
+                };
+                var result = userManager.CreateAsync(bob, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userManager.AddClaimsAsync(bob, new Claim[]{
+                new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                new Claim(JwtClaimTypes.GivenName, "Bob"),
+                new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                new Claim(JwtClaimTypes.Email, "BobSmith@email.com"),
+                new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json),
+                new Claim("location", "somewhere")
+            }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+                Log.Debug("bob created");
+            }
+            else
+            {
+                Log.Debug("bob already exists");
+            }
+        }
+
+        private static void SeedConfigurations(IConfigurationDbContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                Log.Debug("Clients being populated");
+                foreach (var client in Config.Clients.ToList())
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                Log.Debug("Clients already populated");
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                Log.Debug("IdentityResources being populated");
+                foreach (var resource in Config.Ids.ToList())
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                Log.Debug("IdentityResources already populated");
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                Log.Debug("ApiResources being populated");
+                foreach (var resource in Config.Apis.ToList())
+                {
+                    context.ApiResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            else
+            {
+                Log.Debug("ApiResources already populated");
+            }
+        }
+    }
+}
