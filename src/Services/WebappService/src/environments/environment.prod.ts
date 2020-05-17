45f48f0303443a0300d28282c5512f2ac38a1d50
Marcus Gasberg <mrgasberg@hotmail.com>
2020-05-17 13:54:47 +0200

    Fix proxy and env use

--- src/Services/WebappService/src/environments/environment.prod.ts
+++ src/Services/WebappService/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  authority: '/account_api',
+  clientId: 'angularClient',
+  redirectUri: 'http://localhost:4200',
+  responseType: 'id_token token',
+  scope:
+    'openid profile taxingController paymentController stockMarketController api1',
+  taxingControllerUrl: '',
+  paymentControllerUrl: '',
+  stockMarketController: '',
 };
